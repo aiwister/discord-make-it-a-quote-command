a6277b1e1e9934962e940b6cc3d7ce1a8215f27e
YutoYuto990 <yitengyaoxiang4@gmail.com>
2022-06-06 01:41:26 +0900

    update

--- main.py
+++ main.py
@@ -1,47 +1,66 @@
 import discord
 from discord.ext import commands
+from PIL import Image, ImageDraw, ImageFont
+import requests
+import textwrap
 client=commands.Bot(command_prefix="/")
 @client.command()
-async def make_quote(ctx):
+async def makequote(ctx,cmd=None):
     msg=ctx.message.reference
     id=msg.message_id
     message=await ctx.fetch_message(id)
     ico=message.author.avatar_url
     url = ico
     file_name = "icon.jpeg"
+    await ctx.reply("生成しています")
+
     response = requests.get(url)
     image = response.content
+
     with open(file_name, "wb") as aaa:
       aaa.write(image)
-    def adjust(content):
-      msg=""
-      for con in content:
-        if len(msg)%9==8:
-          msg+="\n"
-        msg+=con
-      return msg
+    if message.content.replace("\n","").isascii():
+        para = textwrap.wrap(message.clean_content, width=26)
+    else:
+        para = textwrap.wrap(message.clean_content, width=13)
     icon=Image.open("icon.jpeg")
     haikei=Image.open("grad.jpeg")
     black=Image.open("black.jpeg")
     w,h=(680,370)
-    haikei=haikei.convert("L")
+    w1,h1=icon.size
     haikei=haikei.resize((w,h))
     black=black.resize((w,h))
-    new=Image.new(mode="L",size=(w,h))
     icon=icon.resize((h,h))
+    if cmd=="color":
+      haikei=haikei.convert("L")
+      new=Image.new(mode="RGBA",size=(w,h))
+      icon=icon.convert("RGBA")
+      black=black.convert("RGBA")
+    if not cmd:
+      haikei=haikei.convert("L")
+      new=Image.new(mode="L",size=(w,h))
+      icon=icon.convert("L")
+      black=black.convert("L")
     icon=icon.crop((40,0,680,370))
-    icon=icon.convert("L")
     new.paste(icon)
-    black=black.convert("L")
     sa=Image.composite(new,black,haikei)
     draw = ImageDraw.Draw(sa)# im上のImageDrawインスタンスを作る
-    fnt = ImageFont.truetype('nijimi.otf',40)
-    cont=message.content
-    author=message.author
-    draw.text((350,120),adjust(cont),font=fnt,fill='#FFF') #fontを指定
+    fnt = ImageFont.truetype('higashiume.ttf',28) 
+    w2,h2=draw.textsize("a",font=fnt)
+    i=(int(len(para)/2)*w2)+len(para)*5
+    current_h, pad = 120-i, 0
+    for line in para:
+      if message.content.replace("\n","").isascii():
+        w3,h3=draw.textsize(line.ljust(int(len(line)/2+11)," "),font=fnt)
+        draw.text((11*(w - w3) / 13+10, current_h+h2), line.ljust(int(len(line)/2+11)," "), font=fnt,fill="#FFF")
+      else:
+        w3,h3=draw.textsize(line.ljust(int(len(line)/2+5),"　"),font=fnt)
+        draw.text((11*(w - w3) / 13+10, current_h+h2), line.ljust(int(len(line)/2+5),"　"), font=fnt,fill="#FFF")
+      current_h += h3 + pad#fontを指定
     dr=ImageDraw.Draw(sa)
-    font = ImageFont.truetype('nijimi.otf',25)
-    dr.text((400,300),str(author),font=font,fill="#FFF")
+    font = ImageFont.truetype('nijimi.otf',15)
+    authorw,authorh=dr.textsize(f"-{str(message.author)}",font=font)
+    dr.text((480-int(authorw/2),current_h+h2+10),f"-{str(message.author)}",font=font,fill="#FFF")
     sa.save("te.png")
     file=discord.File("te.png")
     await ctx.send(file=file)
